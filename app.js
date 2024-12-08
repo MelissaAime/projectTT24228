bd3ae61df0b32aedb642b219fd93b4c8d5334c1e
Melissa Aime <aimemelissa1@gmail.com>
2024-12-08 11:03:47 -0300

    cambio de imagenes

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 function generarTarjetas(datos) {
     const contenedor = document.getElementById('contenedor-tarjetas');
     
-    datos.forEach(vino => {
+    datos.forEach(producto => {
         const card = document.createElement('div');
         card.classList.add('card', 'text-center', 'col', 'm-3', 'p-0');
         card.style.width = '18rem';
     
         const img = document.createElement('img');
         img.classList.add('card-img-top');
-        img.src = vino.imagen;
-        img.alt = vino.nombre;
+        img.src = producto.imagen;
+        img.alt = producto.nombre;
     
         const cardBody = document.createElement('div');
         cardBody.classList.add('card-body');
     
         const title = document.createElement('h5');
         title.classList.add('card-title');
-        title.textContent = vino.nombre;
+        title.textContent = producto.nombre;
     
         const text = document.createElement('p');
         text.classList.add('card-text');
-        text.textContent = vino.descripcion;
+        text.textContent = producto.descripcion;
     
         const button = document.createElement('a');
         button.classList.add('btn');
